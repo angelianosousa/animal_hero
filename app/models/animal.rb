0f3a5668121a63f86d1226d2f43ed4bdeac50423
Angeliano <angeliano.sousa@gmail.com>
2022-04-07 17:24:14 -0300

    Adicionado um scope method para que o usuário só visualize os animais de suas ongs cadastradas

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,8 @@
 class Animal < ApplicationRecord
   enum situation: %w[Abandonado Propriedade]
   belongs_to :ong
+
+  scope :standard_scope, ->(current_user){ 
+    where(ong_id: current_user.ongs.ids).includes(:ong)
+  }
 end
